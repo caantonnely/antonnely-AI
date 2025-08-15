1dba28419512b2a02df055b86e4840dbe9fc5c89
caantonnely <142521120+caantonnely@users.noreply.github.com>
2025-08-15 12:34:59 -0300

    Add files via upload

---
+++ antonnely-AI-main/antonnely-AI-main/script.js
@@ -0,0 +1,25 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResutado=document.querySelector(".texto-resultado");
+
+const perguntas =[
+    {
+        enunciado:"Assim que saiu do colégio voce se depara com uma nova tecnologia, no chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios hiper-realistas. Qual é seu primeiro pensamento?",
+        alternativas:[
+            {
+            texto:"Isso é assustador!",
+            afirmacao:"No inicio, eu ficaria com medo do que essa tecnologia pode fazer."
+  },
+  {
+    texto:"isso pode ser incrivel!",
+    afirmacao:"Sempre quis saber como usar a IA no meu dia a dia."
+  }
+        ]
+
+
+        }
+    
+
+    ]
